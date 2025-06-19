c57b2c433b7180c28600d3020ceeb716761827e4
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-19 15:35:21 +0530

    Delete output directory

--- output/programs/sum_list.java
+++
@@ -1,9 +0,0 @@
-  public class SumList {
-      public static int sumList(int[] nums) {
-          if (nums.length == 0) {
-              return 0;
-          }
-
-          return nums[0] + sumList(Arrays.copyOfRange(nums, 1, nums.length));
-      }
-  }
