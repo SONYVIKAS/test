887b7e85d0be0b2d37339b22a65d5b251df4102e
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-19 15:18:07 +0530

    Add converted file for programs/binary_search.py

---
+++ output/programs/binary_search.java
@@ -0,0 +1,20 @@
+public static int binarySearch(int val) {
+    assert 0 < val && val < 101 : "Val must be between 1 and 100";
+
+    int numGuesses = 0;
+    int guess = 0;
+    int low = 0;
+    int high = 101;
+
+    while (guess!= val) {
+        numGuesses++;
+        guess = (high - low) / 2 + low;
+
+        if (guess > val) {
+            high = guess;
+        } else if (guess < val) {
+            low = guess;
+        }
+    }
+
+    return numGuesses;
