976423aae419a48c4e73fefb5f51f56f2294503e
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-20 14:46:41 +0530

    Add converted file for programs/transpose_of_matrix.py

---
+++ output/programs/transpose_of_matrix.java
@@ -0,0 +1,30 @@
+public class Matrix {
+    private List<List<Integer>> mat;
+    
+    public Matrix(List<List<Integer>> mat) {
+        this.mat = mat;
+    }
+    
+    public List<List<Integer>> getMatrix() {
+        return mat;
+    }
+    
+    public List<List<Integer>> transpose() {
+        if (mat!= null) {
+            try {
+                List<List<Integer>> transposedMatrix = new ArrayList<>();
+                for (int i = 0; i < mat.get(0).size(); i++) {
+                    List<Integer> row = new ArrayList<>();
+                    for (List<Integer> column : mat) {
+                        row.add(column.get(i));
+                    }
+                    transposedMatrix.add(row);
+                }
+                return transposedMatrix;
+            } catch (Exception e) {
+                return List.of("Failed to convert transpose because", e.getMessage());
+            }
+        } else {
+            return null;
+        }
+    }
