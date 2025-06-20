acde11f298c4406dfc5e4bed77c8abe11703f4fc
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-20 16:45:09 +0530

    Add converted file for task_handler/task2.py

---
+++ output/task_handler/task2.java
@@ -0,0 +1,28 @@
+public class TruckLoadingContainer {
+    private int weight;
+    private int width;
+    private int length;
+    private int height;
+
+    public TruckLoadingContainer(int weight, int width, int length, int height) {
+        this.weight = weight;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
