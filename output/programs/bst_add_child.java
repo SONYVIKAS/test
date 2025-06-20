f5106c3bdc9e4d4e4db9e86fa6d15b28b7915a3c
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-20 16:35:18 +0530

    Add converted file for programs/bst_add_child.py

---
+++ output/programs/bst_add_child.java
@@ -0,0 +1,26 @@
+public class Node {
+    private int data;
+    private Node left;
+    private Node right;
+
+    public Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void insert(int new_data) {
+        if (new_data < this.data) {
+            if (this.left == null) {
+                this.left = new Node(new_data, null, null);
+            } else {
+                this.left.insert(new_data);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = new Node(new_data, null, null);
+            } else {
+                this.right.insert(new_data);
+            }
+        }
+    }
