5e716e6835f2a16fa9c8bfbc61c755bce5239291
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-06-20 17:23:22 +0530

    Delete output directory

--- output/programs/reverse_string.java
+++
@@ -1,6 +0,0 @@
-  public static String reverseString(String string) {
-      if (string.isEmpty()) {
-          return string;
-      }
-      return reverseString(string.substring(1)) + string.charAt(0);
-  }
