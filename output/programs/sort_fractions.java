829eea5de1e84a1b0bbdd9614b4990c7b6115d52
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-07-01 17:03:31 +0530

    Add converted file for programs/sort_fractions.py

---
+++ output/programs/sort_fractions.java
@@ -0,0 +1,10 @@
+
+```java
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.text.DecimalFormat;
+
+public class SortFractional {
+    public static void main(String[] args) {
+        Random random = new Random();
