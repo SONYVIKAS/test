4644fd9ae47448319766a81d716446fb478cf522
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-07-01 17:02:11 +0530

    Add converted file for programs/print_recursively.py

---
+++ output/programs/print_recursively.java
@@ -0,0 +1,15 @@
+  public class PrintRecursively {
+      public static void printRecursively(int[] lst) {
+          if (lst.length == 0) {
+              return;
+          }
+
+          System.out.println(lst[0]);
+          printRecursively(Arrays.copyOfRange(lst, 1, lst.length));
+      }
+
+      public static void main(String[] args) {
+          int[] lst = {1, 2, 3};
+          printRecursively(lst);
+      }
+  }
