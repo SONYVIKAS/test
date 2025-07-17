b8e395528e52db57a199e0602cdb5d7e6734f562
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-07-17 11:13:27 +0530

    Delete output directory

--- output/programs/sort_dict_by_value.java
+++
@@ -1,8 +0,0 @@
-data = {
-    'one':1,
-    'two':2,
-    'four':4,
-    'three':3
-}
-
-sort_dict = {key:val for key,val in sorted(data.items(),key= lambda x : x[1]) }
--- output/task_handler/task1.java
+++
@@ -1,9 +0,0 @@
-public void unloadVessel(Vessel vessel) {
-    for (Cargo cargo : vessel.getCargos()) {
-        if (cargo.isDangerous()) {
-            ship.getDangerousCargo().add(cargo);
-        } else {
-            ship.getRegularCargo().add(cargo);
-        }
-    }
-    vessel.getCargos().clear();
