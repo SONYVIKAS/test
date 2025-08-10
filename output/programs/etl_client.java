9610305e804d156d50c981fd855973325995680f
SONYVIKAS <77048205+SONYVIKAS@users.noreply.github.com>
2025-08-10 14:06:45 +0530

    Transformed programs/etl_client.py to Java

--- output/programs/etl_client.java
+++ output/programs/etl_client.java
@@ -1,52 +1,48 @@
 import java.util.*;
 
-// Exception class for RetryImmediatelyError
+// Define custom exception for RetryImmediatelyError
 class RetryImmediatelyError extends Exception {
     public RetryImmediatelyError(String errorMessage) {
         super(errorMessage);
     }
 }
 
-// DocumentService interface
-interface DocumentService {
-    Map<String, Object> handle_request() throws RetryImmediatelyError;
-}
-
-// ETLClient class
+// Define the ETLClient class
 public class ETLClient {
-    public String run(DocumentService service, int max_requests) {
-        List<Map<String, Object>> documents = new ArrayList<>();
 
+    public String run(DocumentService service, int maxRequests) throws RetryImmediatelyError {
+        List<Map<String, Object>> documents = new ArrayList<>();
         Map<String, Object> docMap = new HashMap<>();
         docMap.put("doc-count", 0);
         docMap.put("error-count", 0);
-        docMap.put("docs", new HashMap<String, List<String>>());
-
+        docMap.put("docs", new HashMap<>());
         documents.add(docMap);
 
-        for (int i = 0; i < max_requests; i++) {
+        for (int i = 0; i < maxRequests; i++) {
             while (true) {
                 try {
-                    Map<String, Object> event = service.handle_request();
+                    Map<String, Object> event = service.handleRequest();
 
                     if (event.get("operation").equals("add")) {
-                        String doc_id = (String) ((Map<String, Object>) event.get("document")).get("id");
-                        List<String> doc_data = this.remove_words((String) ((Map<String, Object>) event.get("document")).get("data"));
+                        String docId = (String) ((Map<String, Object>) event.get("document")).get("id");
+                        List<String> docData = removeWords((String) ((Map<String, Object>) event.get("document")).get("data"));
 
-                        ((Map<String, List<String>>) documents.get(0).get("docs")).put(doc_id, doc_data);
+                        ((Map<String, List<String>>) documents.get(0).get("docs")).put(docId, docData);
                         documents.get(0).put("doc-count", (Integer) documents.get(0).get("doc-count") + 1);
                     }
 
                     if (event.get("operation").equals("update")) {
-                        String doc_id = (String) ((Map<String, Object>) event.get("document")).get("id");
-                        List<String> doc_data = this.remove_words((String) ((Map<String, Object>) event.get("document")).get("data"));
+                        String docId = (String) ((Map<String, Object>) event.get("document")).get("id");
+                        List<String> docData = removeWords((String) ((Map<String, Object>) event.get("document")).get("data"));
 
-                        ((Map<String, List<String>>) documents.get(0).get("docs")).put(doc_id, doc_data);
+                        ((Map<String, List<String>>) documents.get(0).get("docs")).put(docId, docData);
                     }
 
                     if (event.get("operation").equals("delete")) {
-                        String doc_id = (String) event.get("document-id");
-                        ((Map<String, List<String>>) documents.get(0).get("docs")).remove(doc_id);
+                        String docId = (String) event.get("document-id");
+                        Map<String, List<String>> docs = (Map<String, List<String>>) documents.get(0).get("docs");
+                        docs.remove(docId);
+
                         documents.get(0).put("doc-count", (Integer) documents.get(0).get("doc-count") - 1);
                     }
                 } catch (RetryImmediatelyError e) {
@@ -57,14 +53,14 @@ public class ETLClient {
             }
         }
 
-        return new Gson().toJson(documents.get(0));
+        return new JSONObject(documents.get(0)).toString();
     }
 
-    private List<String> remove_words(String string) {
-        Set<String> remove_words = new HashSet<>(Arrays.asList("and", "or", "not", "but", "to", "in"));
+    public List<String> removeWords(String string) {
+        Set<String> removeWords = new HashSet<>(Arrays.asList("and", "or", "not", "but", "to", "in"));
         List<String> words = new ArrayList<>(Arrays.asList(string.toLowerCase().split(" ")));
 
-        words.removeIf(remove_words::contains);
+        words.removeIf(removeWords::contains);
 
         return words;
     }
